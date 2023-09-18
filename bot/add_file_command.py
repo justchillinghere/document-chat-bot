917298f80c52e67c19a524a6418b8d447c660dba
justchillinghere <41962623+justchillinghere@users.noreply.github.com>
2023-09-18 16:11:22 +0700

    fix

--- bot/add_file_command.py
+++ bot/add_file_command.py
@@ -1,7 +1,7 @@
 from telegram.ext import CommandHandler, ContextTypes
 from telegram import File, Update, Document
 import typing
-
+from langchain.document_loaders import PyPDFLoader
 
 async def add_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> File | None:
 
@@ -12,22 +12,3 @@ async def add_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> File |
 	file = await context.bot.get_file(update.message.document.file_id)
 	loader = PyPDFLoader(file.file_path)
 	pages = loader.load_and_split()
-
-    # message = update.message
-    # if "document" in message.effective_attachment.get_file():
-    #     document: Document = update.message.effective_attachment["document"]
-    #     if document.mime_type == "application/pdf":
-    #         file = document.get_file(document.file_id)
-    #         context.bot.send_message(
-    #             chat_id=update.effective_chat.id,
-    #             text="File uploaded and processed successfully.",
-    #         )
-    #         print(file)
-    #     else:
-    #         context.bot.send_message(
-    #             chat_id=update.effective_chat.id, text="Please upload a PDF file."
-    #         )
-    # else:
-    #     context.bot.send_message(
-    #         chat_id=update.effective_chat.id, text="Please upload a file."
-    #     )
