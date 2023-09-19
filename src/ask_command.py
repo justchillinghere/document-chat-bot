87f52fc73c76974ef1349adc195c6832fad50f7e
justchillinghere <41962623+justchillinghere@users.noreply.github.com>
2023-09-19 13:06:05 +0700

    vectordb works

--- src/ask_command.py
+++ src/ask_command.py
@@ -2,6 +2,9 @@ from PDF_handlerlangchain import Dialog
 
 async def ask(update, context):
 	question_to_ask = update.message.text
+	await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Let me think..."
+    )
 	reply = Dialog(update.message.from_user.id).ask(question_to_ask)
 	await context.bot.send_message(
         chat_id=update.effective_chat.id, text=reply
