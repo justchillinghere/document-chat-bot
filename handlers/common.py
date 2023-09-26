0cb73879c2999e7ef877c605d3844aeee01b7d64
justchillinghere <41962623+justchillinghere@users.noreply.github.com>
2023-09-26 21:12:17 +0700

    added new collection and states

---
+++ handlers/common.py
@@ -0,0 +1,25 @@
+from aiogram import F, Router
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, ReplyKeyboardRemove
+
+router = Router()
+
+
+@router.message(Command(commands=["start"]))
+async def cmd_start(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(
+        text="Вы можете создать новую коллекцию документов или продолжить диалог с существующей",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
+@router.message(Command(commands=["cancel"]))
+@router.message(F.text.lower() == "отмена")
+async def cmd_cancel(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(
+        text="Действие отменено",
+        reply_markup=ReplyKeyboardRemove()
+    )
